b4ad32b8d41859cf8ac8cfa1729957cb3ad392c8
Arvinder <ArvinderDhan@gmail.com>
2023-09-17 03:10:11 -0500

    feat: show off string literals.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-fn say_hello(u: String) {
-    // This returns the `()` type implicitly.
-    // unlike java, all functions return a value here.
-    // The difference is, `()`, or the empty tuple is what
-    // functions return by default.
+fn say_hello(u: &str) {
+    // we now take in a reference to a string, or a "string slice"
+    // rather then a string itself. This is a pointer, and by
+    // using this, we don't have to copy data around (or move it).
     println!("Hello, {u}!");
 }
 
 fn main() {
-    say_hello(String::from("john"));
+    // string literals are by default a reference. This is because
+    // instead of *allocating* memory, they're just pointers to the
+    // relevant section in the binary in memory. (It's the same
+    // reason string literals are a char* or `string_view` in C++).
+    say_hello("john");
 }
