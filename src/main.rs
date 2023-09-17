1222642a882ed809343fa7f45ceaa520ec79f660
Arvinder <ArvinderDhan@gmail.com>
2023-09-17 03:06:21 -0500

    feat: go over functions.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-fn main() {
-    let u = "john";
+fn say_hello(u: String) {
+    // This returns the `()` type implicitly.
+    // unlike java, all functions return a value here.
+    // The difference is, `()`, or the empty tuple is what
+    // functions return by default.
     println!("Hello, {u}!");
+}
+
+fn main() {
+    say_hello(String::from("john"));
 }
